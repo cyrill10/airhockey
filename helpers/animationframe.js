b5f56d2ead79d39aac79f3cc24c5eff5c335189c
cyrill10 <cyrill10@users.noreply.github.com>
2019-08-27 08:54:23 +0200

    Added missing helpers

---
+++ helpers/animationframe.js
@@ -0,0 +1,10 @@
+// animation frame helper
+const requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+const cancelAnimationFrame =
+  window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+
+export { requestAnimationFrame, cancelAnimationFrame };
